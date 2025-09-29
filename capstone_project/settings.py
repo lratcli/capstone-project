f9b52808df69c3de1812e1a1e7aedde64fc0d8de
lracli <220789043+lratcli@users.noreply.github.com>
2025-09-29 20:10:09 +0100

    Add procfile, add hosts and safe for deloy debug code, move key to env.py. Ready for initial web deploy

--- capstone_project/settings.py
+++ capstone_project/settings.py
@@ -31,7 +31,8 @@ if not SECRET_KEY:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com',
+                 "127.0.0.1",]
 
 
 # Application definition
