033d67b014f88fde2aa0f0756b15b5182145d228
lracli <220789043+lratcli@users.noreply.github.com>
2025-10-01 19:48:37 +0100

    add HypotheticalSystemDetailView function to spec_a_console.views.py - untested

--- spec_a_console/views.py
+++ spec_a_console/views.py
@@ -12,9 +12,27 @@ def index(request):
 
 
 class HypotheticalSystemListView(generic.ListView):
+    """A view to list all approved HypotheticalSystem instances."""
     model = HypotheticalSystem
     template_name = 'spec_a_console/index.html'
     context_object_name = 'hypothetical_systems'
     paginate_by = 10
     queryset = HypotheticalSystem.objects.filter(
         approval=1).order_by('-created_on')
+
+
+def HypotheticalSystemDetailView(request, slug):
+    """A view to show details of a specific HypotheticalSystem."""
+    queryset = HypotheticalSystem.objects.filter(approval=1)
+    system = get_object_or_404(queryset, slug=slug)
+
+    # TODO: add reviews of the system here
+    # reviews = system.reviews.all().order_by('-created_on')
+
+    return render(
+        request,
+        'spec_a_console/system_detail.html',
+        {
+            'system': system,
+            # 'reviews': reviews,
+        })
