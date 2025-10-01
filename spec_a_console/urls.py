f943dd30893d5a310593404cb142e848f68611f7
lracli <220789043+lratcli@users.noreply.github.com>
2025-10-01 21:27:21 +0100

    add navigation between basic html pages index and system_detailed

--- spec_a_console/urls.py
+++ spec_a_console/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     # path('', views.index, name='index'),
     path('', views.HypotheticalSystemListView.as_view(), name='index'),
+    path('<slug:slug>/', views.hypothetical_system_detail_view, 
+         name='system_detail'),
 ]
